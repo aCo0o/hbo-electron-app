a0239b4474374ac68c7c01f5f1a061dc96d3901f
Aleksandar Cincarevic <a.cincarevic@Aleksandars-MacBook-Pro.local>
2019-05-19 22:54:29 +0200

    moved files to bin

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 console.log('1. [ Starting HBO App ]');
 
 const { app } = require('electron');
-const init = require('./init');
+const init = require('./bin/init');
 
 app.on('ready', init);
 app.on('window-all-closed', ()=> { if (process.platform !== 'darwin') app.quit();  });
